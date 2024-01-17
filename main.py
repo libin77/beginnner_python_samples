6968d8a94446c2a543ed6c173a05e2c9481e60db
libin77 <53886464+libin77@users.noreply.github.com>
2024-01-17 08:33:19 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+#Fulllname Printer
+fname = input("What's your first name?\n")
+mname = input("What's your middle name?\n")
+lname = input("What's your last name?\n")
+print("Your full name is "+ fname + " " + mname + " " + lname).  explain
