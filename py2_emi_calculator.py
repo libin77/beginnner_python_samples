7bfa20a02563b5379c48188f7a3d7e6c3b3db5b1
libin77 <53886464+libin77@users.noreply.github.com>
2024-01-18 08:14:42 +0530

    Create py2_emi_calculator.py

---
+++ py2_emi_calculator.py
@@ -0,0 +1,12 @@
+#EMI Calculator
+#[P x R x (1+R)^N]/[(1+R)^N-1] 
+principal_amt = input("What is the total loan amount? Rs ")
+rate_of_intr = input("What is the rate of interest? ")
+num_months = input("Select tenure in months ")
+
+#calculation
+rate  = float(rate_of_intr)/12/100
+compunding_factor = (1+rate)**int(num_months)
+emi = (int(principal_amt) * rate * compunding_factor) / (compunding_factor - 1)
+
+print(f"Your montly emi will be Rs {round(emi,2)}")
