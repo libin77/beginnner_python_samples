4974a762bf1831c91bf1457bb93613f4db4d8071
libin77 <53886464+libin77@users.noreply.github.com>
2024-01-19 08:59:33 +0530

    Update py1_variables_mng_fullname_printer.py

--- py1_variables_mng_fullname_printer.py
+++ py1_variables_mng_fullname_printer.py
@@ -1,5 +1,6 @@
-#Fulllname Printer
-fname = input("What's your first name?\n")
-mname = input("What's your middle name?\n")
-lname = input("What's your last name?\n")
-print("Your full name is "+ fname + " " + mname + " " + lname).  explain
+def fullname_printer():
+  #Fulllname Printer
+  fname = input("What's your first name?\n")
+  mname = input("What's your middle name?\n")
+  lname = input("What's your last name?\n")
+  print("Your full name is "+ fname + " " + mname + " " + lname).  explain
